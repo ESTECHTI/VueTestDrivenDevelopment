264aa0a8629e9c34c1aa9fcbb9c7d12dfad8f44e
ESTECHTI <e.s.tech@outlook.com>
2022-03-15 11:18:58 -0300

    Including units test the first time

---
+++ source/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <SignUpPage />
+</template>
+
+<script>
+import SignUpPage from './pages/SignUpPage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    SignUpPage
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ source/src/pages/SignUpPage.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <h1>Sign Up</h1>
+    <label for="username">Username</label>
+    <input id="username" type="text" placeholder="username"/>
+
+    <label for="email">E-mail</label>
+    <input id="email" type="email" placeholder="e-mail"/>
+
+    <label for="password">Password</label>
+    <input id="password" type="password" placeholder="password"/>
+
+    <label for="password-repeat">Password Repeat</label>
+    <input id="password-repeat" type="password" placeholder="password"/>
+
+    <button disabled>Sign Up</button>
+  </div>
+</template>
+ 
+<script>
+ 
+export default {
+  name: 'SignUpPage',
+  components: {
+ 
+  },
+  props: {
+ 
+  },
+  data:() => {
+    return {
+ 
+    }
+  },
+  computed: {
+ 
+  },
+  methods: {
+ 
+  }
+ 
+}
+</script>
+ 
+<style lang="scss" scoped>
+ 
+</style>
