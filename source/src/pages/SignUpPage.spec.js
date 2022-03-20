0217fb719f0714a6a5026548fc3f7d5edf682879
ESTECHTI <e.s.tech@outlook.com>
2022-03-20 17:54:33 -0300

    Mocking

--- source/src/pages/SignUpPage.spec.js
+++ source/src/pages/SignUpPage.spec.js
@@ -1,55 +1,112 @@
 /* eslint-disable no-undef */
-import SignUpPage from './SignUpPage.vue'
-import {render, screen} from '@testing-library/vue'
-import "@testing-library/jest-dom"
+import SignUpPage from "./SignUpPage.vue";
+import { render, screen } from "@testing-library/vue";
+import "@testing-library/jest-dom";
+import userEvent from "@testing-library/user-event";
+//import axios from "axios";
+import { setupServer } from "msw/node";
+import { rest } from "msw";
 
 describe("Sign Up Page", () => {
   describe("Layout", () => {
-    it('has Sign Up Header', () => {
+    it("has Sign Up Header", () => {
       render(SignUpPage);
       const header = screen.queryByRole("heading", { name: "Sign Up" });
-      expect(header).toBeInTheDocument()
+      expect(header).toBeInTheDocument();
     });
     it("has username input", () => {
       render(SignUpPage);
       const input = screen.queryByLabelText('Username');
-      expect(input).toBeInTheDocument();
+      expect(input).toBeInTheDocument()
     });
     it("has email input", () => {
       render(SignUpPage);
-      const inputCount = screen.queryByLabelText('E-mail');
-      expect(inputCount).toBeInTheDocument();
+      const input = screen.queryByLabelText('E-mail');
+      expect(input).toBeInTheDocument()
     });
     it("has password input", () => {
       render(SignUpPage);
-      const inputCount = screen.queryByLabelText('Password');
-      expect(inputCount).toBeInTheDocument();
-    })
+      const input = screen.queryByLabelText('Password');
+      expect(input).toBeInTheDocument()
+    });
     it("has password type for password input", () => {
       render(SignUpPage);
       const input = screen.queryByLabelText('Password');
-      expect(input.type).toBe("password");
-    })
-    it("has password repeat input", () => {
+      expect(input.type).toBe("password")
+    });
+    it("has password password repeat input", () => {
       render(SignUpPage);
-      const inputCount = screen.queryByLabelText('Password Repeat');
-      expect(inputCount).toBeInTheDocument();
-    })
+      const input = screen.queryByLabelText('Password Repeat');
+      expect(input).toBeInTheDocument()
+    });
     it("has password type for password repeat input", () => {
       render(SignUpPage);
       const input = screen.queryByLabelText('Password Repeat');
-      expect(input.type).toBe("password");
-    })
-    it('has Sign Up button', () => {
-      render(SignUpPage);
-      const button = screen.queryByRole("button", { name: "Sign Up" });
-      expect(button).toBeInTheDocument()
+      expect(input.type).toBe("password")
     });
-    it('hdisables button initially', () => {
+    it("has Sign Up button", () => {
       render(SignUpPage);
       const button = screen.queryByRole("button", { name: "Sign Up" });
-      expect(button).toBeDisabled()
+      expect(button).toBeInTheDocument();
+    });
+    it("has Sign Up button disabled", () => {
+      render(SignUpPage);
+      const button = screen.queryByRole("button", { name: "Sign Up" });
+      expect(button).toBeDisabled();
     });
   });
-});
+  describe('Interactions', () => {
+    it("enables the button when the password and password repeat fields have the same value", async () => {
+      render(SignUpPage);
+      const usernameIput = screen.queryByLabelText("Username");
+      const emailInput = screen.queryByLabelText("E-mail");
+      const passwordInput = screen.queryByLabelText("Password");
+      const passwordRepeatInput = screen.queryByLabelText("Password Repeat");
+      await userEvent.type(usernameIput, "user1");
+      await userEvent.type(emailInput, "user1@mail.com");
+      await userEvent.type(passwordInput, "P4ssword");
+      await userEvent.type(passwordRepeatInput, "P4ssword");
+      const button = screen.queryByRole("button", { name: "Sign Up" });
+      expect(button).toBeEnabled();
+    });
+    it("sends username, email and password to backend after clicking the button", async () => {
+      let requestBody;
+      const server = setupServer(
+        rest.post("/api/1.0/users", (req, res, ctx) => {
+          requestBody = req.body;
+          return res(ctx.status(200));
+        })
+      );
 
+      server.listen()
+
+      render(SignUpPage);
+      const usernameInput = screen.queryByLabelText("Username");
+      const emailIput = screen.queryByLabelText("E-mail");
+      const passwordInput = screen.queryByLabelText("Password");
+      const passwordRepeatInput = screen.queryByLabelText("Password Repeat");
+      await userEvent.type(usernameInput, "user1");
+      await userEvent.type(emailIput, "user1@mail.com");
+      await userEvent.type(passwordInput, "P4ssword");
+      await userEvent.type(passwordRepeatInput, "P4ssword");
+      const button = screen.queryByRole("button", { name: "Sign Up" });
+
+      // const mockFn = jest.fn();
+      //axios.post = mockFn;
+      // window.fetch = mockFn;
+
+      await userEvent.click(button);
+
+      await server.close();
+
+      // const firstCall = mockFn.mock.calls[0];
+      // const body = JSON.parse(firstCall[1].body);
+
+      expect(requestBody).toEqual({
+        username: 'user1',
+        email: 'user1@mail.com',
+        password: 'P4ssword'
+      });
+    });
+  })
+})
--- source/src/pages/SignUpPage.vue
+++ source/src/pages/SignUpPage.vue
@@ -1,23 +1,26 @@
 <template>
-  <div>
+  <form>
     <h1>Sign Up</h1>
     <label for="username">Username</label>
-    <input id="username" type="text" placeholder="username"/>
+    <input id="username" type="text" placeholder="username" v-model="username"/>
 
     <label for="email">E-mail</label>
-    <input id="email" type="email" placeholder="e-mail"/>
+    <input id="email" type="email" placeholder="e-mail" v-model="email"/>
 
     <label for="password">Password</label>
-    <input id="password" type="password" placeholder="password"/>
+    <input id="password" type="password" placeholder="password" v-model="password"/>
 
     <label for="password-repeat">Password Repeat</label>
-    <input id="password-repeat" type="password" placeholder="password"/>
+    <input id="password-repeat" type="password" placeholder="password" v-model="passwordRepeat"/>
 
-    <button disabled>Sign Up</button>
-  </div>
+    <button :disabled="isDisabled" @click.prevent="submit">Sign Up</button>
+
+    {{username}}
+  </form>
 </template>
  
 <script>
+import axios from "axios";
  
 export default {
   name: 'SignUpPage',
@@ -29,16 +32,26 @@ export default {
   },
   data:() => {
     return {
- 
+      username: '',
+      email: '',
+      password: '',
+      passwordRepeat: '',
     }
   },
   computed: {
- 
+    isDisabled() {
+      return (this.password && this.passwordRepeat)? this.password != this.passwordRepeat : true
+    }
   },
   methods: {
- 
+    submit() {
+      axios.post("/api/1.0/users", {
+        username: this.username,
+        email: this.email,
+        password: this.password,
+      });
+    }
   }
- 
 }
 </script>
  
